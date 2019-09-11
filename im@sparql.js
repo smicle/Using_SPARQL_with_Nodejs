b2552108e5c1d90da4b8b706d0b843508fe61be8
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2019-09-11 18:17:40 +0900

    im@sparql.jsを追加

---
+++ im@sparql.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+
+const endPoint = 'https://sparql.crssnky.xyz/spql/imas/query'
+
+const Query = `
+PREFIX schema: <http://schema.org/>
+PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
+SELECT ?o ?h
+WHERE {
+  ?s schema:name|schema:alternateName ?o;
+     schema:height ?h.
+}order by(?h)
+`
+
+const fetchSPARQL = () =>
+  fetch(`${endPoint}?output=json&query=${encodeURIComponent(Query)}`)
+    .then(r => r.json())
+    .then(r => r.results.bindings)
+;(async () => {
+  const json = await fetchSPARQL()
+  json.forEach(v => console.log(v.o, v.h))
+})().catch(e => console.error(e))
