2aea2e95fb4431a569e4b5f99cfe76f598051502
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2019-09-25 19:28:48 +0900

    アイドルとcvを紐付いて取得する

--- im@sparql.js
+++ im@sparql.js
@@ -3,30 +3,29 @@ const fetch = require('node-fetch')
 const endPoint = 'https://sparql.crssnky.xyz/spql/imas/query'
 const output = 'json'
 const query = `
-PREFIX schema: <http://schema.org/>
 PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
 
-SELECT ?name ?cv
+SELECT ?name ?cv ?title
 WHERE {
-  ?s schema:name ?name;
+  ?s rdfs:label ?name;
      imas:cv ?cv.
-  FILTER (lang(?name) = 'ja').
   FILTER (lang(?cv) = 'ja').
 }
-LIMIT 1000
 `
 
 const fetchSPARQL = query =>
   fetch(`${endPoint}?output=${output}&query=${encodeURIComponent(query)}`)
     .then(r => r.json())
     .then(r => r.results.bindings)
+
 ;(async () => {
   const json = await fetchSPARQL(query)
 
-  const list = json.map(v => ({
-    name: v.name.value,
-    cv: v.cv.value,
-  }))
-
-  list.forEach((v, i) => console.log(i, v))
+  json
+    .map(v => ({
+      name: v.name.value,
+      cv: v.cv.value,
+    }))
+    .forEach(v => console.log(v))
 })().catch(e => console.error(e))
