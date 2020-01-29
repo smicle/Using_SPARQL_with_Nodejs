475576f595efee0707dc19092b38889a8d4fba4e
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-01-29 18:49:49 +0900

    add adjustTitleを追加した

--- im@sparql/im@sparql.ts
+++ im@sparql/im@sparql.ts
@@ -10,7 +10,8 @@ PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
 SELECT ?name ?cv ?title
 WHERE {
   ?s rdfs:label ?name;
-     imas:cv ?cv.
+    imas:cv ?cv;
+    imas:Title ?title.
   FILTER (lang(?cv) = 'ja').
 }
 `
@@ -19,9 +20,24 @@ const fetchSPARQL = (query: string) =>
   fetch(`${endPoint}?output=${output}&query=${encodeURIComponent(query)}`)
     .then(r => r.json())
     .then(r => r.results.bindings)
+
+// prettier-ignore
+const adjustTitle = (title: string) =>
+  title === 'CinderellaGirls' ? '346'   :
+  title === 'MillionStars'    ? '765ML' :
+  title === '315ProIdols'     ? '315'   :
+  title === '283Pro'          ? '283'   :
+  title === 'DearlyStars'     ? '876'   :
+  title === '1054Pro'         ? '1054'  :
+  title
+;
 ;(async () => {
   const json = await fetchSPARQL(query)
-  const idol = json.map(v => ({name: v.name.value, cv: v.cv.value}))
+  const idol = json.map(v => ({
+    name: v.name.value,
+    cv: v.cv.value,
+    title: adjustTitle(v.title.value),
+  }))
   fs.writeFileSync('im@sparql.json', JSON.stringify(idol))
   console.log('I wrote in it.')
 })().catch(e => console.error(e))
